5773ef28b6b74c16c16a3de04f095d204a80ce1b
akun <akun@youkundeair.lan>
2017-07-03 09:48:36 +0800

    新建工程，第一次提交

---
+++ javaShell/src/main/java/cn/dface/javaShell/App.java
@@ -0,0 +1,13 @@
+package cn.dface.javaShell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
